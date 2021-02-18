e8470a78cd159cf1705c0766910a60ae048612d2
Walidnurudin <walidnurudin47@gmail.com>
2021-02-18 08:17:25 +0700

    tailwind config, add poppins font

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,7 +14,11 @@ export default {
       { hid: 'description', name: 'description', content: '' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      {
+        rel: 'stylesheet',
+        href: 'https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap'
+      },
     ]
   },
 
